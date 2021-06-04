3c616ec5b7c02f23617345d7ad60bf90856132dd
Roman Kulyniak <roman_kulyniak@epam.com>
2021-06-04 14:25:01 +0300

    implement basic web socket connection

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/codenjoyme/codenjoy-go-client/engine"
 
+func main() {
+	// *** paste here board page url from a browser after registration ***
+	engine.CreateWebSocketConnection("http://localhost:8080/codenjoy-contest/board/player/0?code=000000000000")
 }
