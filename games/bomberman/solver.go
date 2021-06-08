447111b467540857c4bea707754904a238215d41
Roman Kulyniak <roman_kulyniak@epam.com>
2021-06-08 15:48:08 +0300

    [clients][go] pass game & url through command line arguments

--- games/bomberman/solver.go
+++ games/bomberman/solver.go
@@ -1,25 +1,32 @@
 package bomberman
 
 import (
-    "fmt"
-    "github.com/codenjoyme/codenjoy-go-client/engine"
+	"fmt"
+	"github.com/codenjoyme/codenjoy-go-client/engine"
 )
 
 type Solver struct {
+	B *Board
 }
 
-func (s Solver) Get(b *Board) Action {
-    printDetailedBoardInfo(b)
-    // make your action
-    return MoveFire(engine.UP)
+func (s Solver) Get(rawBoard []rune) string {
+	s.B.UpdateBoard(rawBoard)
+	fmt.Printf("\nBoard:\n%s\n", s.B.BoardAsString())
+	fmt.Printf("Hero at: %v\n", s.B.GetHero())
+	fmt.Printf("Other heroes at: %v\n", s.B.GetOtherHeroes())
+	fmt.Printf("Meat choppers at: %v\n", s.B.GetMeatChoppers())
+	fmt.Printf("Destroy walls at: %v\n", s.B.GetDestroyableWalls())
+	fmt.Printf("Bombs at: %v\n", s.B.GetBombs())
+	fmt.Printf("Blasts at: %v\n", s.B.GetBlasts())
+	fmt.Printf("Expected blasts at: %v\n", s.B.GetFutureBlasts())
+
+	answer := s.nextStep()
+	fmt.Println("Answer: " + answer)
+	fmt.Println("-------------------------------------------------------------")
+	return string(answer)
 }
 
-func printDetailedBoardInfo(b *Board) {
-    fmt.Printf("Hero at: %v\n", b.GetHero())
-    fmt.Printf("Other heroes at: %v\n", b.GetOtherHeroes())
-    fmt.Printf("Meat choppers at: %v\n", b.GetMeatChoppers())
-    fmt.Printf("Destroy walls at: %v\n", b.GetDestroyableWalls())
-    fmt.Printf("Bombs at: %v\n", b.GetBombs())
-    fmt.Printf("Blasts at: %v\n", b.GetBlasts())
-    fmt.Printf("Expected blasts at: %v\n", b.GetFutureBlasts())
+func (s Solver) nextStep() Action {
+	// make your action
+	return MoveFire(engine.UP)
 }
