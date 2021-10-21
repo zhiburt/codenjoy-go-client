4f0ec8d2d8e605094847d5e725e627b079098192
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-21 21:22:01 +0300

    [clients][all] Added /Q to all remove stuff command.

--- 1-download-env.bat
+++ 1-download-env.bat
@@ -13,6 +13,9 @@ if "%INSTALL_LOCALLY%"=="false" ( goto :skip )
 if "%INSTALL_LOCALLY%"=="" ( goto :skip )
 
 cd %ROOT%
+IF EXIST %TOOLS%\go.zip (
+    del /Q %TOOLS%\go.zip
+)
 powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%ARCH_GO%','%TOOLS%\go.zip') }"
 rd /S /Q %TOOLS%\..\.golang
 %ARCH% x -y -o%TOOLS%\.. %TOOLS%\go.zip
