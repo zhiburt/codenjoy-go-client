bc03e550250c78bdf06400b1427519aee9b0e842
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-20 15:22:18 +0200

    [clients][go][bash][docker] Used docker instead of portable.

--- 0-settings.sh
+++ 0-settings.sh
@@ -10,27 +10,4 @@ eval_echo "[[ \"$BOARD_URL\" == \"\" ]]   && BOARD_URL=http://127.0.0.1:8080/cod
 
 eval_echo "ROOT=$PWD"
 
-eval_echo "[[ \"$SKIP_TESTS\" == \"\" ]]  && SKIP_TESTS=true"
-
-eval_echo "TOOLS=$ROOT/.tools"
-eval_echo "ARCH=tar"
-
-# Set to true if you want to ignore go installation on the system
-eval_echo "[[ \"$INSTALL_LOCALLY\" == \"\" ]]   && INSTALL_LOCALLY=true"
-
-eval_echo "[[ \"$INSTALL_LOCALLY\" == "true" ]] && export GOPATH="
-
-eval_echo "[[ \"$GOPATH\" == \"\" ]]    && NO_GO=true"
-eval_echo "[[ \"$NO_GO\" == \"true\" ]] && export GOPATH=$ROOT/.golang"
-eval_echo "[[ \"$NO_GO\" == \"true\" ]] && export PATH=$GOPATH/bin:$PATH"
-
-eval_echo "GO=$GOPATH/bin/go"
-
-color $COLOR4 "PATH=$PATH"
-color $COLOR4 "GOPATH=$GOPATH"
-echo
-
-eval_echo "ARCH_URL=https://dl.google.com/go/go1.17.3.linux-amd64.tar.gz"
-eval_echo "ARCH_FOLDER=go"
-
 eval_echo "GO_CLIENT_HOME=$ROOT"
