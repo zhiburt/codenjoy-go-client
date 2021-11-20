9e84fce7745b95f895f0bdcc9477c128cdf38625
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-20 15:20:52 +0200

    [clients][go] Added information where the variables are taken from.

--- main.go
+++ main.go
@@ -6,16 +6,26 @@ import (
 	"github.com/codenjoyme/codenjoy-go-client/games/mollymage"
 	"github.com/codenjoyme/codenjoy-go-client/games/sample"
 	"os"
+	"fmt"
 )
 
 func main() {
 	game := "mollymage"
 	url := "http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000"
+    source := "Runner"
 
 	if len(os.Args) == 3 {
 		game = os.Args[1]
 		url = os.Args[2]
+        source = "Environment"
 	}
+
+	fmt.Printf("Got from %s:\n" +
+            "\t 'GAME': '%s'\n" +
+            "\t 'URL':  '%s'\n",
+            source,
+            game,
+            url)
 
 	engine.NewWebSocketRunner(url).Run(determineGameSolver(game))
 }
